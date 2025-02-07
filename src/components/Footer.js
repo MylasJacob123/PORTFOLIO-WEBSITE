5539079d9f5806dcd869fe5ee439a8f1ef2aa309
MylasJacob123 <mylasjacob18.5@gmail.com>
2025-02-07 11:40:27 +0200

    Universal selector add to App.css to fix app spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,30 +13,7 @@ function Footer() {
       <div className="footer-copywright">
         <h3>Copyright © {year} LAJM</h3>
       </div>
-      <div className="footer-body">
-        <ul className="footer-icons">
-          <li className="social-icons">
-            <a
-              href="https://github.com/MylasJacob123"
-              target="_blank"
-              rel="noreferrer"
-              className="icon-colour home-social-icons"
-            >
-              GitHub
-            </a>
-          </li>
-          <li className="social-icons">
-            <a
-              href="https://www.linkedin.com/in/jacob-mylas-5057ba340/"
-              target="_blank"
-              rel="noreferrer"
-              className="icon-colour home-social-icons"
-            >
-              LinkedIn
-            </a>
-          </li>
-        </ul>
-      </div>
+      
     </div>
   );
 }
